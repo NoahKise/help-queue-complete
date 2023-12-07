8458f14a04137d4d0f868de4dc0319956e7a0557
noahkise <noah@kisefamily.com>
2023-12-06 22:19:15 -0800

    end of wednesday lesson

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,11 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+import ReusableForm from "./ReusableForm";
 
 function NewTicketForm(props){
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.issue.value);
+  }
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTicketCreation({
+      names: event.target.names.value, 
+      location: event.target.location.value, 
+      issue: event.target.issue.value, 
+      id: v4()
+    });
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <ReusableForm 
+        formSubmissionHandler={handleNewTicketFormSubmission}
+        buttonText="Help!" />
     </React.Fragment>
   );
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
